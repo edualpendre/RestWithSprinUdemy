183281e646b8ec4679c20b0c187ca79990bb1a71
Edu <edualpendre@gmail.com>
2021-02-18 20:16:25 -0300

    Include Download and Upload Files

--- src/main/java/com/edualpendre/restWithSpring/RestWithSpringApplication.java
+++ src/main/java/com/edualpendre/restWithSpring/RestWithSpringApplication.java
@@ -1,11 +1,14 @@
 package com.edualpendre.restWithSpring;
 
+import com.edualpendre.restWithSpring.config.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageConfig.class})
 @EnableAutoConfiguration
 @ComponentScan
 public class RestWithSpringApplication {
